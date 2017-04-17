ebc5beb5fde905e6c458cb976c9de4802e76a01f
Derrick Bray <derrickbray@yahoo.com>
2017-04-17 10:22:56 -0500

    add root url to universes

--- nested_routes/config/routes.rb
+++ nested_routes/config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+
+  get 'universes/index'
+  root 'universes#index'
+
   resources :universes do
     resources :heros
   end
+
   resources :universes do
     resources :demi_gods
   end
+
   resources :universes do
     resources :gods
   end
+
   resources :universes do
     resources :myths
   end
